a1c9cb9444eeb7d3d9594658eaf7a666c3a9acc6
mmisiewicz <mateusz.misiewicz@cairo.pl>
2019-05-27 11:25:27 +0200

    Add name

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,6 +14,7 @@ class DefaultController extends AbstractController
 
         return $this->render('index.html.twig', [
             'number' => 'COS',
+            'name' => 'Mateusz',
         ]);
     }
 }
